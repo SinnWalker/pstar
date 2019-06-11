fcceb43f63834f4bbf2a45de233bb3fdfccab57f
Sina Samarbafzadeh <sinasa2@gmail.com>
2019-06-11 08:09:34 +0200

    problem solved but revision needed

--- src/Client.java
+++ src/Client.java
@@ -11,50 +11,90 @@ import static java.util.stream.Collectors.toMap;
 public class Client {
     private Map<String, Integer> wordCounter;
 
-    public Client(){
+    public Client() {
 
         wordCounter = new HashMap<>();
         String host = "127.0.0.1";
-        int port = 13456;
 
-        try(Socket socket = new Socket(host, port);
-            Scanner in = new Scanner(
-                    new InputStreamReader(socket.getInputStream()));){
+        final Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                int port = 13456;
+
+                try (Socket socket = new Socket(host, port);
+                     Scanner in = new Scanner(
+                             new InputStreamReader(socket.getInputStream()));) {
 
 
-            while (in.hasNext()) {
-                String word = in.next().trim().toLowerCase();
-                System.out.println(word);
-                if(wordCounter.containsKey(word)){
-                    wordCounter.put(word, wordCounter.get(word) + 1);
-                }
-                else {
-                    wordCounter.put(word, 1);
+                    while (in.hasNext()) {
+                        String word = in.next().trim().toLowerCase();
+                        if (wordCounter.containsKey(word)) {
+                            wordCounter.put(word, wordCounter.get(word) + 1);
+                        } else {
+                            wordCounter.put(word, 1);
+                        }
+                    }
+                } catch (UnknownHostException e) {
+                    System.err.println("Unknown host " + host);
+                    System.exit(1);
+                } catch (IOException e) {
+                    System.err.println("Couldn't get I/O for the connection to " +
+                            host);
+                    System.exit(1);
                 }
             }
-             Map<String, Integer> sorted = wordCounter
-                     .entrySet()
-                     .stream()
-                     .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                     .limit(5)
-                     .collect(
-                             toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
-                                     LinkedHashMap::new));
-            for(String s : sorted.keySet()){
-                System.out.println(s + " " + wordCounter.get(s));
+        };
+
+        thread1.run();
+
+        Thread thread2 = new Thread() {
+            @Override
+            public void run() {
+                int port = 13457;
+
+                try (Socket socket = new Socket(host, port);
+                     Scanner in = new Scanner(
+                             new InputStreamReader(socket.getInputStream()));) {
+
+
+                    while (in.hasNext()) {
+                        String word = in.next().trim().toLowerCase();
+                        if (wordCounter.containsKey(word)) {
+                            wordCounter.put(word, wordCounter.get(word) + 1);
+                        } else {
+                            wordCounter.put(word, 1);
+                        }
+                    }
+                } catch (UnknownHostException e) {
+                    System.err.println("Unknown host " + host);
+                    System.exit(1);
+                } catch (IOException e) {
+                    System.err.println("Couldn't get I/O for the connection to " +
+                            host);
+                    System.exit(1);
+                }
             }
-        }
-        catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + host);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    host);
-            System.exit(1);
+        };
+        thread2.run();
+
+        Map<String, Integer> sorted = wordCounter
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(5)
+                .collect(
+                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                                LinkedHashMap::new));
+
+        System.out.println("Word" + "\t\t" + "Count");
+        System.out.println();
+
+        for (String s : sorted.keySet()) {
+            System.out.println(s + "\t\t\t" + wordCounter.get(s));
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Client client = new Client();
     }
 }
